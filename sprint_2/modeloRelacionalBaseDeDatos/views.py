b88ae5cf0537df54c1d740406034b05b762f5715
Andres2293 <carlosandresrojas1070@gmail.com>
2022-09-16 21:16:51 -0500

    HU02- se crearon los Views Empleados y Rol

--- sprint_2/modeloRelacionalBaseDeDatos/views.py
+++ sprint_2/modeloRelacionalBaseDeDatos/views.py
@@ -1,19 +1,21 @@
+#from django.shortcuts import render
 import json
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from modeloRelacionalBaseDeDatos.models import Empresas,Empleado,Reporte_contable
+from modeloRelacionalBaseDeDatos.models import Empresas,Empleado,Reporte_contable,Rol
 from django.http import JsonResponse
 
 
-#from django.shortcuts import render
 
-# Create your views here.
+#____________________TABLA EMPRESAS______________________
 
 class EmpresasView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
+        #FUNCION PARA OBTENER EMPRESAS
     
     def get(self,request,id_empresas=""):
 
@@ -34,6 +36,8 @@ class EmpresasView(View):
 
         return JsonResponse(datos)
 
+    
+            #FUNCION PARA CREAR EMPRESAS
 
     def post(self,request):
             data=json.loads(request.body)
@@ -41,6 +45,8 @@ class EmpresasView(View):
             empresa.save()
             datos={'mensaje': 'Empresa Registrada Exitosamente'}
             return JsonResponse(datos)
+
+            #FUNCION PARA EDITAR EMPRESAS
         
     def put(self,request,id_empresas):
             data=json.loads(request.body)
@@ -60,7 +66,81 @@ class EmpresasView(View):
                 mensaje={"mensaje":"No se encontro la empresa"}
             
             return JsonResponse(mensaje)
+
+#______________________TABLA ROL______________________
+
+
+class RolView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request,*args,**kwargs)
+
+        #FUNCION PARA OBTENER UN ROL
+    
+    def get(selft,request,Id_rol=""):
+
+        if len(Id_rol)>0:
+            rol=list(Rol.object.filter(id_rol=Id_rol).values())
+            if len(rol)>0:
+                datos={'Rol':rol}
+
+            else:
+                datos={'mensaje': "No se encontró el Rol."}
         
+        else:
+            Roles=list(Rol.objects.values())
+            if len(Roles)>0:
+                datos={"mensaje":Roles}
+            else:
+                datos={"mensaje":"No se encontro el Rol."}
+
+        return JsonResponse(datos)
+
+    
+
+#____________________TABLA EMPLEADOS______________________
+    
+class EmpleadoView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+
+        #FUNCION PARA OBTENER EMPLEADOS      
+
+    def get(self,request,idempleado=""):
+
+        if len(idempleado)>0:
+            empleado=list(Empleado.objects.filter(id_empleado=idempleado).values())
+            if len(empleado)>0:
+                datos={'Empleado': empleado}
+            else:
+                datos={'mensaje': 'No se encontraron empleados'}
+        else:
+            empleado=list(Empleado.objects.values()) 
+            if len(empleado)>0:
+                datos={"mensaje":empleado}
+            else:
+                datos={"mensaje":"No se encontraron empleados."}
+
+        return JsonResponse(datos)
+
+
+        #FUNCION PARA AGREGAR EMPLEADOS
+
+    def post (self,request):
+        data=json.loads(request.body)
+        print(data)
+        roles=Rol.objects.get(id_rol=data["rol"])
+        empre=Empresas.objects.get(id_empresas=data["empresas_id"])
+        emple=Empleado.objects.create(id_empleado=data["id_empleado"],empresas_id=empre, rol=roles,nombre=data['nombre'],apellido=data['apellido'],email=data['email'],telefono=data['telefono'],fecha_creacion=data['fecha_creacion'])
+        emple.save()
+        mensaje={"Mensaje":"Empleado registrado exitosamente"}
+
+        return JsonResponse(mensaje)
+        
+        #FUNCION PARA EDITAR EMPLEADOS
+
     def put(self,request,id_empleado):
             data=json.loads(request.body)
             empleado=list(Empleado.objects.filter(Id_empleado=id_empleado).values())
@@ -79,12 +159,15 @@ class EmpresasView(View):
             
             return JsonResponse(mensaje)
 
+#____________________TABLA REPORTE CONTABLE______________________
+
 class Reporte_contableView(View):
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
+        #FUNCION PARA OBTENER REPORTE CONTABLE
 
     def get(self,request,idestado=""):
 
@@ -102,3 +185,4 @@ class Reporte_contableView(View):
                 datos={"mensaje":"No se encontro ningun Reporte."}
 
         return JsonResponse(datos)
+
