2e38fdd5265586b9add2665d9a35e4c43727e0ac
camtamayo <111544710+camtamayo@users.noreply.github.com>
2022-09-25 20:31:48 -0500

    Delete sprint_2 directory

--- sprint_2/modeloRelacionalBaseDeDatos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ModelorelacionalbasededatosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'modeloRelacionalBaseDeDatos'
